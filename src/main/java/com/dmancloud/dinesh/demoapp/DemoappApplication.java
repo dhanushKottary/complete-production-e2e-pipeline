6d86d050a7c58ac2330ec1a15b4fc1481ce5d66e
dhanushKottary <dhanush2819kottary@gmail.com>
2025-06-01 16:37:35 +0530

    Updated the application java file

--- src/main/java/com/dmancloud/dinesh/demoapp/DemoappApplication.java
+++ src/main/java/com/dmancloud/dinesh/demoapp/DemoappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoappApplication {
 
     public static void main(String[] args) {
+    	System.out.println("Running the application....");
         SpringApplication.run(DemoappApplication.class, args);
     }
 }
